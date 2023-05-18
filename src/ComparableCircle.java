5857f3ff5d4b0b335efa871f3b22867cd53c3af8
HoangHong <piiyk172@gmail.com>
2023-05-18 15:55:44 +0700

    First commit

--- src/ComparableCircle.java
+++ src/ComparableCircle.java
@@ -1,20 +1,10 @@
-public class ComparableCircle extends Circle implements Comparable<ComparableCircle>{
+import java.util.Comparator;
+public class ComparableCircle implements Comparator<Circle>{
 
-    public ComparableCircle(){
-
-    }
-
-    public ComparableCircle(double radius) {
-        super(radius);
-    }
-
-    public ComparableCircle(double radius, String color, boolean filled){
-        super(radius,color,filled);
-    }
     @Override
-    public int compareTo(ComparableCircle o) {
-        if (getRadius() > o.getRadius()) return 1;
-        else if (getRadius() < o.getRadius()) return -1;
-        else return 1;
+    public int compare(Circle c1, Circle c2) {
+        if (c1.getRadius() > c2.getRadius()) return 1;
+        else if (c1.getRadius() < c2.getRadius()) return -1;
+        else return 0;
     }
 }
--- src/ComparableCircleTest.java
+++ src/ComparableCircleTest.java
@@ -1,21 +1,23 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class ComparableCircleTest {
     public static void main(String[] args) {
-        ComparableCircle[] circles = new ComparableCircle[3];
-        circles[0] = new ComparableCircle(3.6);
-        circles[1] = new ComparableCircle();
-        circles[2] = new ComparableCircle(4.5,"Yellow",false);
+        Circle[] circles = new Circle[3];
+        circles[0] = new Circle(3.6);
+        circles[1] = new Circle();
+        circles[2] = new Circle(3.5, "indigo", false);
 
         System.out.println("Pre-sorted:");
-        for (ComparableCircle circle : circles) {
+        for (Circle circle : circles) {
             System.out.println(circle);
         }
 
-        Arrays.sort(circles);
+        Comparator circleComparator = new ComparableCircle();
+        Arrays.sort(circles, circleComparator);
 
         System.out.println("After-sorted:");
-        for (ComparableCircle circle : circles) {
+        for (Circle circle : circles) {
             System.out.println(circle);
         }
     }
